40a83e6c109a428f431741c793f0cbdedaab9365
tomlingb <tomlingb@rose-hulman.edu>
2017-12-16 17:00:51 -0500

    Working on it

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -2,8 +2,8 @@
 A   Baby   class and methods that use the Baby class.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Geoffrey Tomlinson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -90,10 +90,17 @@ def main():
 #
 ########################################################################
 
+class Baby(object):
 
+    def __init__(self, name):
+        self.name = name
+        print('Hello baby', self.name)
 
+    def feed_baby(self):
+        print('Thank you for feeding baby', self.name)
 
-
+    def hour_passes(self):
+        
 
 
 # ----------------------------------------------------------------------
