f6613dc050fdaccc980068acb8483a98939ae33a
tomlingb <tomlingb@rose-hulman.edu>
2017-12-16 17:55:12 -0500

    Finished

--- src/m2_baby_class.py
+++ src/m2_baby_class.py
@@ -39,7 +39,7 @@ def main():
 
 
 # ----------------------------------------------------------------------
-# TODO: 2. Implement a class called   Baby   that has a constructor and
+# DONE: 2. Implement a class called   Baby   that has a constructor and
 #   two methods, as described below.  Your finished Baby class should
 #   cause the code above to display the expected output.  Hint: Your
 #   class will need instance variables that you must figure out.
@@ -94,16 +94,28 @@ class Baby(object):
     def __init__(self, name):
         self.name = name
         print('Hello baby', self.name)
+        self.count = 0
 
     def feed_baby(self):
         print('Thank you for feeding baby', self.name)
+        self.count = 0
 
     def hour_passes(self):
+        if self.count == 0:
+            print('Baby', self.name, ' is sleeping.')
+        elif self.count == 1:
+            print('Baby', self.name, ' is awake, Time for food.')
+        else:
+            print('Baby', self.name, ' is CRYING uncontrollably! Feed the '
+                                     'baby!')
+        self.count = self.count + 1
 
 
 # ----------------------------------------------------------------------
 # If this module is running at the top level (as opposed to being
 # imported by another module), then call the 'main' function.
 # ----------------------------------------------------------------------
+
+
 if __name__ == '__main__':
     main()
